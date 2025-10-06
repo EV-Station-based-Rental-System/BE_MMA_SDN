c1c7df4a0abda57ea310401f55d44a8addc1ff2c
nnh53 <nnh53.work@gmail.com>
2025-10-06 16:24:06 +0700

    test: test

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello: string {
     return this.appService.getHello();
+  }
+
+  @Get('test')
+  getTest(): string {
+    return this.appService.getTest();
   }
 }
