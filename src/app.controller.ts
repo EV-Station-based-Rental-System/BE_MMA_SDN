8e5f418bd2ee785ddbf26bc3c056ff3b2016bdcd
nnh53 <nnh53.work@gmail.com>
2025-10-06 16:31:45 +0700

    style: fix

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,8 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello: string {
-    return this.appService.getHello();
+  getHello(name: string): string {
+    return this.appService.getHello(name);
   }
 
   @Get('test')
