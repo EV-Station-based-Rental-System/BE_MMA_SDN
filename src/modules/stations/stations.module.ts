ca547fb6fb5312356e2f8ff7ee66dc02d4329580
nnh53 <nnh53.work@gmail.com>
2025-10-11 02:26:04 +0700

    feat: add MongooseModule imports for stations and vehicles modules

--- src/modules/stations/stations.module.ts
+++ src/modules/stations/stations.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Station, StationSchema } from "src/models/station.schema";
 import { JwtAuthGuard } from "src/common/guards/jwt.guard";
 import { RolesGuard } from "src/common/guards/roles.guard";
 import { StationsService } from "./stations.service";
 import { StationsController } from "./stations.controller";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Station.name, schema: StationSchema }])],
   controllers: [StationsController],
   providers: [StationsService, JwtAuthGuard, RolesGuard],
 })
--- src/modules/vehicles/vehicles.module.ts
+++ src/modules/vehicles/vehicles.module.ts
@@ -1,10 +1,13 @@
 import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Vehicle, VehicleSchema } from "src/models/vehicle.schema";
 import { JwtAuthGuard } from "src/common/guards/jwt.guard";
 import { RolesGuard } from "src/common/guards/roles.guard";
 import { VehiclesService } from "./vehicles.service";
 import { VehiclesController } from "./vehicles.controller";
 
 @Module({
+  imports: [MongooseModule.forFeature([{ name: Vehicle.name, schema: VehicleSchema }])],
   controllers: [VehiclesController],
   providers: [VehiclesService, JwtAuthGuard, RolesGuard],
 })
