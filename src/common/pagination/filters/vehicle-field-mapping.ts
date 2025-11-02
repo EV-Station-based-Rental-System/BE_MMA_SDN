d4e540e81f5263c4924fe42c10ad2245dd6ff3a1
Mac Viet Thong <thongmvse181966@fpt.edu.vn>
2025-11-03 00:46:05 +0700

    feat: add status search by params for vehicles entity

--- src/common/pagination/filters/vehicle-field-mapping.ts
+++ src/common/pagination/filters/vehicle-field-mapping.ts
@@ -1,13 +1,13 @@
 import { FilterField } from "src/common/utils/type";
 
 export const VehicleFieldMapping: Record<string, FilterField> = {
-  // make, model
+  // make, model, status
   search: {
     field: "search",
     type: "string",
     customWhere: (value: string) => {
       const regex = new RegExp(value, "i");
-      return [{ make: regex }, { model: regex }];
+      return [{ make: regex }, { model: regex }, { status: regex }];
     },
   },
   model_year: {
@@ -17,5 +17,9 @@ export const VehicleFieldMapping: Record<string, FilterField> = {
   is_active: {
     field: "is_active",
     type: "boolean",
+  },
+  status: {
+    field: "status",
+    type: "string",
   },
 };
